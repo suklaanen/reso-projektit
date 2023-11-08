e0ea1b7fad6f0172667855e8aadb52d20276c8c0
Juho Hietala <juhohietala90@gmail.com>
2023-11-08 13:44:46 +0200

    Models ja controllers pohjat luotu. SQL tiedosto lisatty

---
+++ backend/controllers/account.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const account = require('../models/account_model');
+
+router.get('/', function(request, response) {
+    
+});
+
+router.get('/:id', function(request, response) {
+    
+});
+
+router.post('/', function(request, response) {
+    
+});
+
+router.put('/:id', function(request, response) {
+    
+});
+
+router.delete('/:id', function(request, response) {
+    
+});
