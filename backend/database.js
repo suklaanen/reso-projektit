e267fca772102880353aa92dba45d94ebeb8b8bc
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2023-11-11 11:03:10 +0200

    Reverted database.js to original ver

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,8 @@
 const mysql = require("mysql2");
-const db = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'atm_user',
-  password: 'pankkipass',
-  database: 'bank_automat'
+const connection = mysql.createPool( {
+    host: '127.0.0.1',
+    user: 'atm_user',
+    password: 'pankkipass',
+    database: 'bank_automat'
 });
-
-module.exports = db;
+module.exports = connection;
