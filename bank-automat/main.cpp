ed6300d316c8a7c93f29d061cd58fc401804b46f
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2023-11-13 15:37:54 +0200

    created bankstyle.qss - work in progress with paths...

--- bank-automat/main.cpp
+++ bank-automat/main.cpp
@@ -1,10 +1,27 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
+#include <QDebug>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QDir::setCurrent("ABSOLUUTTINENPOLKUTÄHÄN/group_1/bank-automat");
+
+    QFile styleFile("styles/bankstyle.qss");
+
+    qDebug() << "Current working directory: " << QDir::currentPath();
+    if (styleFile.open(QFile::ReadOnly | QFile::Text)) {
+        QString style = QLatin1String(styleFile.readAll());
+        a.setStyleSheet(style);
+        styleFile.close();
+    } else {
+        qDebug() << "Failed to open a stylesheet file.";
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
