ee7537fe009ccd43771cc8ebfc9118fc7d57035d
Juho Hietala <juhohietala90@gmail.com>
2023-11-16 12:34:59 +0200

    Login.cpp kommentointi

--- bank-automat/login.cpp
+++ bank-automat/login.cpp
@@ -9,7 +9,7 @@ Login::Login(QWidget *parent) :
     cardType = "";
     manager = new QNetworkAccessManager(this);
     reply = nullptr;
-    loginTimer = new QTimer;
+    loginTimer = new QTimer(this);
     loginTimer->setSingleShot(false);
 }
 
@@ -18,80 +18,85 @@ Login::~Login()
 
 }
 
+//Ottaa vastaan kortin ID:n mainwindow:sta
 void Login::setCardID(QString inputCardID)
 {
     cardID = inputCardID;
-    requestCardID();
+    requestCardID(); // Kutsutaan metodia, josta lähtee pyyntö REST APIlle kortin tyypin tarkistukselle
 }
 
+//Ottaa vastaan PIN koodin mainwindows:sta
 void Login::setPIN(QString inputPin)
 {
     pin = inputPin;
-    requestLogin();
+    requestLogin();// Kutsutaan metodia, josta lähtee login pyyntö REST APIlle
 }
 
+//Ottaa vastaan ja käsittelee vastauksen kortin tyypin tarkistuksesta REST APIsta
 void Login::handleCard()
 {
     QNetworkReply * reply = qobject_cast<QNetworkReply*>(sender());
 
     if (reply->error() == QNetworkReply::NoError) {
-        // Successful response
+        // Onnistunut vastaus
         QByteArray responseData = reply->readAll();
-        // Process responseData as needed
+        // Käsitellään vastausta
 
         if(responseData == "false") {
-            emit cardFail();
+            emit cardFail(); //Tuntematon kortin ID, lähetetään signaali cardFail() maindwindow:lle
         }
         else {
             cardType = QString(responseData).replace("\"", "");
             qDebug() << "cardType:" << cardType;
-            if (cardID.toInt() < 1) {
-                emit cardFail();
+            if (cardID.toInt() < 1) { //Tarkistetaan, ettei ole annettu tyhjää kortin ID:tä
+                emit cardFail(); //Jos ID on tyhjä -> cardFail() maindwindow:lle
             } else {
                 if(cardType == "credit/debit") {
-                    emit cardOkSelectType();
+                    emit cardOkSelectType(); //Jos kortin tyyppi on yhdistelmäkortti, lähetetään signaali mainwindow:lle, jossa valitaan käytettävä tili
                 } else {
-                    emit cardOk (cardType);
+                    emit cardOk (cardType); //Debit, credit tai admin korteissa lähetetään suoraan signaali cardOk() mainwindow:lle, jossa skipataan tilin valinta
                 }
             }
         }
     } else {
-        // Handle the error
+        // Käsitellään mahdollinen virhe vastauksessa (verkkovirhe)
         qDebug() << "Couldnt get card type" << reply->errorString();
     }
-    // Clean up the reply
+    // Tyhjennetään vastaus myöhemmin
     reply->deleteLater();
 }
 
+//Ottaa vastaan ja käsittelee vastauksen login pyynnöstä.
 void Login::handlePin()
 {
     QNetworkReply * reply = qobject_cast<QNetworkReply*>(sender());
 
     if (reply->error() == QNetworkReply::NoError) {
-        // Successful response
+        // Onnistunut vastaus
         QByteArray responseData = reply->readAll();
-        // Process responseData as needed
+        // Käsitellään vastaus
 
         qDebug() << responseData;
         //if(responseData.length()>20) {
-        if(responseData != "false") {
-            if (cardType == "admin") {
-                emit loginOkAdmin(QString(responseData));
+        if(responseData != "false") { //Jos vastaus on != "false", niin token on vastaanotettu ja kirjautuminen ok
+            if (cardType == "admin") { //Tarkistetaan vasta täällä, onko kortin tyyppi admin, koska seuraavat signaalit vievät käyttäjän tai adminin päävalikkoon
+                emit loginOkAdmin(QString(responseData)); //Lähetetään mainwindow:lle signaali adminin päävalikkoon siirtymiseksi
             } else {
-                emit loginOkUser(QString(responseData));
+                emit loginOkUser(QString(responseData));  //Signaali käyttäjän päävalikkoon siityminen
             }
         }
         else {
-            emit loginFail();
+            emit loginFail(); //Jos tokenia ei tullu, tulee "false" ja signaali loginFail() mainwindow:lle
         }
     } else {
-        // Handle the error
+        // Käsitellään mahdollinen virhe (verkkovirhe)
         qDebug() << "Could not login" << reply->errorString();
     }
-    // Clean up the reply
+    // Tyhjennetään vastaus myöhemmin
     reply->deleteLater();
 }
 
+//Lähettää REST APIin pyynnön kortin tyypin tarkistamiseksi
 void Login::requestCardID()
 {
     QNetworkRequest request;
@@ -100,20 +105,17 @@ void Login::requestCardID()
     connect(reply, SIGNAL(finished()), this, SLOT(handleCard()));
 }
 
+//Lähettää REST APIin login pyynnön
 void Login::requestLogin()
 {
     QNetworkRequest request;
     QJsonObject body;
-    body.insert("id_card",cardID);
-    body.insert("pin",pin);
+    body.insert("id_card",cardID); //Asetetaan request bodyyn id_card
+    body.insert("pin",pin);        //ja pin
     request.setUrl(QUrl("http://localhost:3000/login"));
     request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
     reply = manager->post(request, QJsonDocument(body).toJson());
     connect(reply, SIGNAL(finished()), this, SLOT(handlePin()));
 }
 
-void Login::activate(bool on_off)
-{
-    active = on_off;
-}
 
