5f056380d8ea9bf0dadb82c081656b301925f63d
Juho Hietala <juhohietala90@gmail.com>
2023-11-18 13:23:31 +0200

    Summien valinta, syottaminen seka siirto withdraw luokkaan onnistuu

--- bank-automat/withdraw.cpp
+++ bank-automat/withdraw.cpp
@@ -5,3 +5,17 @@ Withdraw::Withdraw(QObject *parent)
 {
 
 }
+
+void Withdraw::setAmount(QString amount)
+{
+    this->amount = amount;
+    qDebug() << "Amount received: " << this->amount;
+}
+
+void Withdraw::setInfo(QString token, QString accountID, QString cardID, QString cardType)
+{
+    this->token = token;
+    this->accountID = accountID;
+    this->cardID = cardID;
+    this->cardType = cardType;
+}
