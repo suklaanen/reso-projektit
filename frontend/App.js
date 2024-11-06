6c77ee96936b89771ae99bfd16135767426c920c
Rebecca Soisenniemi <t2sokr00@students.oamk.fi>
2024-11-06 14:04:40 +0200

    frontendiin placeholdereita ja navigaatiota

--- frontend/App.js
+++ frontend/App.js
@@ -10,9 +10,11 @@ import CustomTopBar from './components/CustomTopBar';
 import CustomBottomBar from './components/CustomBottomBar';
 import Home from './screens/home/Home';
 import AccountMain from './screens/account/AccountMain';
-import { AccountLoggedIn, AccountLoggedOut } from './screens/account/AccountComponents';
+import { AccountLoggedIn, AccountLoggedOut, AccountMaintain } from './screens/account/AccountComponents';
 import ItemsMain from './screens/items/ItemsMain';
 import Credits from './screens/credits/Credits';
+import MessagesMain from './screens/messages/MessagesMain';
+import { ItemsFromThisUser, QueuesOfThisUser } from './screens/items/ItemComponents';
 
 const Stack = createStackNavigator();
 
@@ -32,6 +34,10 @@ export default function App() {
                 <Stack.Screen name="AccountLoggedOut" component={AccountLoggedOut} options={{ headerShown: false }} />
                 <Stack.Screen name="ItemsMain" component={ItemsMain} options={{ headerShown: false }} />
                 <Stack.Screen name="Credits" component={Credits} options={{ headerShown: false }} />
+                <Stack.Screen name="MessagesMain" component={MessagesMain} options={{ headerShown: false }} />
+                <Stack.Screen name="AccountMaintain" component={AccountMaintain} options={{ headerShown: false }} />
+                <Stack.Screen name="MyItems" component={ItemsFromThisUser} options={{ headerShown: false }} />
+                <Stack.Screen name="MyQueues" component={QueuesOfThisUser} options={{ headerShown: false }} />
               </Stack.Navigator>
             </View>
           <CustomBottomBar />
--- frontend/screens/items/ItemComponents.js
+++ frontend/screens/items/ItemComponents.js
@@ -1,23 +1,51 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { ScrollView } from 'react-native';
-import { AllItems, NoItemsWhenLoggedOut } from './ItemExports';
-
-export const ItemsLoggedIn = () => {
-  const scrollViewRef = useRef(null);
-
-  return (
-    <ScrollView contentContainerStyle={{ padding: 8 }} ref={scrollViewRef}>
-        <AllItems />
-    </ScrollView>
-  );
-};
+import { NoItemsWhenLoggedOut, AllItems, NavigateToThisUsersItems, NavigateToThisUsersQueue, ItemAddNew, ItemModify, ItemDelete, ItemJoinOnQueue } from './FindItems';
+import { BasicSection } from '../../components/CommonComponents';
 
 export const ItemsLoggedOut = () => {
   const scrollViewRef = useRef(null);
 
   return (
-    <ScrollView contentContainerStyle={{ padding: 8 }} ref={scrollViewRef}>
+    <ScrollView contentContainerStyle={{ padding: 8 }}>
         <NoItemsWhenLoggedOut />
     </ScrollView>
   );
 };
+
+export const ItemsLoggedIn = () => {
+  const scrollViewRef = useRef(null);
+
+  return (
+    <ScrollView contentContainerStyle={{ padding: 8 }}>
+        <AllItems />
+        <ItemAddNew />
+        <NavigateToThisUsersItems />
+        <NavigateToThisUsersQueue />
+    </ScrollView>
+  );
+};
+
+export const ItemsFromThisUser = () => {
+  const scrollViewRef = useRef(null);
+
+  return (
+    <ScrollView contentContainerStyle={{ padding: 8 }}>
+      <BasicSection>
+        Käyttäjän omat julkaisut tänne ja niiden muokkaus / poisto / merkintä noudetuksi
+      </BasicSection>
+    </ScrollView>
+  );
+}
+
+export const QueuesOfThisUser = () => {
+  const scrollViewRef = useRef(null);
+
+  return (
+    <ScrollView contentContainerStyle={{ padding: 8 }}>
+      <BasicSection>
+        Käyttäjän omat varaukset/listautumiset tänne ja jonotussijat sekä varauksen peruutus
+      </BasicSection>
+    </ScrollView>
+  );
+}
