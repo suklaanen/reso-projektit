f0fdba32f834a0f9707b1b5f4c4e7fcb2f15f9f4
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-11-15 14:01:31 +0200

    lisätty useEffect, joka hakee käyttäjän keskustelut databasesta ja printtaa dokumenttien idt näkymään

--- frontend/screens/messages/MessagesMain.js
+++ frontend/screens/messages/MessagesMain.js
@@ -1,22 +1,48 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { ScrollView, Text} from 'react-native';
 import { Heading, BasicSection } from '../../components/CommonComponents';
 import { AuthenticationContext } from '../../services/auth';
+import { firestore } from '../../services/firebaseConfig';
+import { doc, onSnapshot, query, where, collection } from 'firebase/firestore';
 
 const MessagesMain = () => {
-    const {authState} = useContext(AuthenticationContext);
+    // const {authState} = useContext(AuthenticationContext);
+    const [threads, setThreads] = useState([]);
+    const sampleUserId = "2vpqcqFcuHNJvhJOldR2"
 
+    useEffect(() => {
+        const threadsRef = collection(firestore, "threadstest");
+        const userRef = doc(firestore, "users", sampleUserId);
 
-    if (!authState) {
-      return <Text>No user data found.</Text>;
-    }
+        const q = query(threadsRef, where('participants', 'array-contains', userRef));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            const threadsData = snapshot.docs.map(doc => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+
+            setThreads(threadsData); 
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    useEffect(() => {
+        console.log(threads);
+    }, [threads]);
 
     return (
         <ScrollView contentContainerStyle={{ padding: 8 }}>
             <Heading title="Omat keskustelut" />
 
             <BasicSection>
-                Tähän käyttäjän aktiiviset keskustelut
+                {threads.length > 0 ? (
+                    threads.map(thread => (
+                        <Text key={thread.id}>{thread.id}{"\n"}</Text>
+                    ))
+                ) : (
+                    <Text>Aktiivisia keskusteluja ei löytynyt</Text>
+                )}
             </BasicSection>
 
         </ScrollView>
