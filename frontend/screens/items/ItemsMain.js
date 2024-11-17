fa5192f0b4719999cd78ca146fa06d6e5c912b06
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-11-17 14:01:04 +0200

    fix context import

--- frontend/screens/items/ItemsMain.js
+++ frontend/screens/items/ItemsMain.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { ItemsLoggedIn, ItemsLoggedOut } from './ItemComponents';
-import { AuthenticationContext } from '../../services/auth';
+import { AuthenticationContext } from "../../context/AuthenticationContext";
 
 const ItemsMain = () => {
     const {authState} = useContext(AuthenticationContext);
