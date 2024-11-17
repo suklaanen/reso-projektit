80b654cd9d155a02b7fd1de24c6813cc4dc61934
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-11-17 14:00:51 +0200

    refactor

--- frontend/screens/account/FindUser.js
+++ frontend/screens/account/FindUser.js
@@ -1,97 +1,75 @@
-import React, { useState, useContext } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth, firestore } from '../../services/firebaseConfig';
-import { query, where, getDocs, collection } from 'firebase/firestore';
-import { Alert, View } from 'react-native';
-import { Heading, AccountSection, CommonText, BasicSection } from '../../components/CommonComponents';
-import { ButtonCancel, ButtonDelete, ButtonConfirm, ButtonContinue, ButtonNavigate } from '../../components/Buttons';
-import { Icon } from 'react-native-elements';
-import { useNavigation } from '@react-navigation/native';
-import { AuthenticationContext } from '../../services/auth.js';
-import { clearUserData, saveUserData } from '../../services/asyncStorageHelper';
-import globalStyles from '../../assets/styles/Styles.js';
-import Toast from 'react-native-toast-message';
+import React, { useState, useContext } from "react";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../services/firebaseConfig";
+import { Alert, View } from "react-native";
+import {
+  Heading,
+  AccountSection,
+  CommonText,
+  BasicSection,
+} from "../../components/CommonComponents";
+import {
+  ButtonCancel,
+  ButtonDelete,
+  ButtonConfirm,
+  ButtonContinue,
+  ButtonNavigate,
+} from "../../components/Buttons";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+import { AuthenticationContext } from "../../context/AuthenticationContext";
+import globalStyles from "../../assets/styles/Styles.js";
+import Toast from "react-native-toast-message";
+import { saveUserToFirestore } from "../../services/firebaseController.js";
 
 export const UserLogin = ({ isVisible, toggleVisible }) => {
-  const [usermail, setLoginUsername] = useState('');
-  const [password, setLoginPassword] = useState('');
+  const [usermail, setLoginUsername] = useState("");
+  const [password, setLoginPassword] = useState("");
   const { setAuthState } = useContext(AuthenticationContext);
   const navigation = useNavigation();
 
-  const handleLoginSuccess = async (data) => {
-    console.log('Kirjautuminen onnistui käyttäjätiedoilla:', data);
-    await saveUserData(data);
-    setAuthState(data);
-  };
-
-  const userLogin = async (credential, password) => {
-    try {
-      let email = credential;
-  
-      if (!credential.includes('@')) {
-        const usersRef = collection(firestore, 'users');
-        const usernameQuery = query(usersRef, where('username', '==', credential));
-        const querySnapshot = await getDocs(usernameQuery);
-  
-        if (querySnapshot.empty) {
-          throw new Error('Käyttäjätunnusta ei löytynyt');
-        }
-  
-        const userDoc = querySnapshot.docs[0];
-        email = userDoc.data().email;
-      }
-  
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      const uid = userCredential.user.uid;
-      const accessToken = await userCredential.user.getIdToken();
-  
-      if (accessToken) {
-        await AsyncStorage.setItem('userId', uid);
-        await AsyncStorage.setItem('accessToken', accessToken);
-      } else {
-        console.warn('accessToken was undefined. Skipping storage of access token.');
-      }
-  
-      return { success: true, userId: uid, accessToken };
-    } catch (error) {
-      Toast.show({
-        type: 'error',
-        text1: 'Kirjautuminen epäonnistui',
-        text2: error.message,
-      });
-      console.error('Kirjautuminen epäonnistui:', error.message);
-      throw error;
-    }
-  };
-  
   const handleLogin = async () => {
-
     try {
-      const data = await userLogin(usermail, password);
-      
+      const data = await signInWithEmailAndPassword(auth, usermail, password);
       if (data) {
-        handleLoginSuccess(data);
+        setAuthState(data);
       } else {
-        Alert.alert('Kirjautuminen epäonnistui', 'Virheellinen käyttäjätunnus tai salasana');
+        Alert.alert(
+          "Kirjautuminen epäonnistui",
+          "Virheellinen käyttäjätunnus tai salasana"
+        );
       }
     } catch (error) {
-      console.error('Login error:', error);
-      Alert.alert('Virhe kirjautumisessa', error.message || 'Yhteysvirhe');
+      console.error("Login error:", error);
+      Alert.alert("Virhe kirjautumisessa", error.message || "Yhteysvirhe");
       Toast.show({
-        type: 'error',
-        text1: 'Kirjautuminen epäonnistui',
-        text2: error.message || 'Yhteysvirhe',
+        type: "error",
+        text1: "Kirjautuminen epäonnistui",
+        text2: error.message || "Yhteysvirhe",
       });
     }
   };
 
   return (
     <View>
-      <Heading title="Kirjaudu sisään" onPress={toggleVisible} style={{ backgroundColor: isVisible ? 'rgba(25, 26, 30, 0.7)' : 'rgba(18, 18, 18, 0.9)' }}/>
-      {isVisible && ( 
+      <Heading
+        title="Kirjaudu sisään"
+        onPress={toggleVisible}
+        style={{
+          backgroundColor: isVisible
+            ? "rgba(25, 26, 30, 0.7)"
+            : "rgba(18, 18, 18, 0.9)",
+        }}
+      />
+      {isVisible && (
         <AccountSection>
-          <CommonText value="Kirjaudu sisään omalla käyttäjätunnuksellasi ja salasanallasi." editable={false} />
+          <CommonText
+            value="Kirjaudu sisään omalla käyttäjätunnuksellasi ja salasanallasi."
+            editable={false}
+          />
           <CommonText
             value={usermail}
             onChangeText={setLoginUsername}
@@ -105,7 +83,7 @@ export const UserLogin = ({ isVisible, toggleVisible }) => {
             trailingIcon={() => <Icon name="lock" />}
             secureTextEntry
           />
-          <ButtonContinue title="Kirjaudu" onPress={handleLogin}/>
+          <ButtonContinue title="Kirjaudu" onPress={handleLogin} />
         </AccountSection>
       )}
     </View>
@@ -113,80 +91,101 @@ export const UserLogin = ({ isVisible, toggleVisible }) => {
 };
 
 export const UserRegister = ({ isVisible, toggleVisible }) => {
-  const [registerUsername, setRegisterUsername] = useState('');
-  const [registerPassword, setRegisterPassword] = useState('');
-  const [registerEmail, setRegisterEmail] = useState('');
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
 
-  const userRegister = async ( email, password, registerUsername ) => {
+  const userRegister = async (email, password, registerUsername) => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const uid = userCredential.user.uid;
-  
+
       Toast.show({
-        type: 'success',
-        text1: 'Käyttäjätunnus luotu!',
-        text2: 'Voit nyt kirjautua!',
+        type: "success",
+        text1: "Käyttäjätunnus luotu!",
+        text2: "Voit nyt kirjautua!",
       });
-  
-    await saveUserToFirestore(uid, registerUsername, email);
-  
-    return { success: true, uid, username: registerUsername, email };
-    
+
+      await saveUserToFirestore(uid, registerUsername, email);
+
+      return { success: true, uid, username: registerUsername, email };
     } catch (error) {
       Toast.show({
-        type: 'error',
-        text1: 'Käyttäjän luominen epäonnistui.',
+        type: "error",
+        text1: "Käyttäjän luominen epäonnistui.",
         text2: error.message,
       });
-      console.error('Rekisteröinnissä tapahtui virhe:', error.message);
+      console.error("Rekisteröinnissä tapahtui virhe:", error.message);
       throw error;
     }
   };
-  
+
   const handleRegister = async () => {
     try {
-      const data = await userRegister(registerEmail, registerPassword, registerUsername);
+      const data = await userRegister(
+        registerEmail,
+        registerPassword,
+        registerUsername
+      );
       if (data) {
-        setRegisterEmail('');
-        setRegisterPassword('');
-        setRegisterUsername('');
+        setRegisterEmail("");
+        setRegisterPassword("");
+        setRegisterUsername("");
       } else {
-        Alert.alert('Rekisteröityminen epäonnistui', 'Virhe rekisteröitymisessä');
+        Alert.alert(
+          "Rekisteröityminen epäonnistui",
+          "Virhe rekisteröitymisessä"
+        );
       }
     } catch (error) {
-      Alert.alert('Tapahtui virhe', error.message || 'Ei yhteyttä palvelimeen');
+      Alert.alert("Tapahtui virhe", error.message || "Ei yhteyttä palvelimeen");
     }
   };
 
   return (
     <>
-      <Heading title="Rekisteröidy" onPress={toggleVisible} style={{ backgroundColor: isVisible ? 'rgba(25, 26, 30, 0.7)' : 'rgba(18, 18, 18, 0.9)' }}/>
-      {isVisible && ( 
+      <Heading
+        title="Rekisteröidy"
+        onPress={toggleVisible}
+        style={{
+          backgroundColor: isVisible
+            ? "rgba(25, 26, 30, 0.7)"
+            : "rgba(18, 18, 18, 0.9)",
+        }}
+      />
+      {isVisible && (
         <AccountSection>
-          <CommonText value="Rekisteröidy palvelun käyttäjäksi täyttämällä pyydetyt kohdat." editable={false} />
           <CommonText
-            placeholder='Käyttäjätunnus'
+            value="Rekisteröidy palvelun käyttäjäksi täyttämällä pyydetyt kohdat."
+            editable={false}
+          />
+          <CommonText
+            placeholder="Käyttäjätunnus"
             value={registerUsername}
             onChangeText={setRegisterUsername}
             editable={true}
             trailingIcon={() => <Icon name="person" />}
-         />
+          />
           <CommonText
-            placeholder='Sähköposti'
+            placeholder="Sähköposti"
             value={registerEmail}
             onChangeText={setRegisterEmail}
             editable={true}
             trailingIcon={() => <Icon name="email" />}
           />
           <CommonText
-            placeholder='Salasana'
+            placeholder="Salasana"
             value={registerPassword}
             onChangeText={setRegisterPassword}
             editable={true}
             trailingIcon={() => <Icon name="lock" />}
             secureTextEntry
           />
-          <ButtonContinue title="Rekisteröidy" onPress={handleRegister}/>
+          <ButtonContinue title="Rekisteröidy" onPress={handleRegister} />
         </AccountSection>
       )}
     </>
@@ -200,21 +199,19 @@ export const DeleteAccountOfThisUser = () => {
   const navigation = useNavigation();
 
   const userDelete = async () => {
-
     try {
       const user = auth.currentUser;
 
       await deleteUserDataFromFirestore(user.uid);
 
       await user.delete();
-      console.log('Käyttäjän autentikointitili poistettu');
-      
-      } catch (error) {
-        Toast.show({
-          type: 'error',
-          text1: 'Poistovirhe',
-          text2: error.message,
-        });
+      console.log("Käyttäjän autentikointitili poistettu");
+    } catch (error) {
+      Toast.show({
+        type: "error",
+        text1: "Poistovirhe",
+        text2: error.message,
+      });
     }
   };
 
@@ -222,9 +219,9 @@ export const DeleteAccountOfThisUser = () => {
     try {
       await userDelete();
       setAuthState(null);
-      Alert.alert('Tilin poisto onnistui');
+      Alert.alert("Tilin poisto onnistui");
     } catch (error) {
-      Alert.alert('Tapahtui virhe', error.message);
+      Alert.alert("Tapahtui virhe", error.message);
     }
   };
 
@@ -241,8 +238,8 @@ export const DeleteAccountOfThisUser = () => {
   const handleConfirmWhenDeleting = () => {
     handleDeleteUser();
     setDeletingThisAccount(true);
-    navigation.navigate('Home');
-  }
+    navigation.navigate("Home");
+  };
 
   const handleDeletingThisAccountCancel = () => {
     setDeletingThisAccount(false);
@@ -255,92 +252,118 @@ export const DeleteAccountOfThisUser = () => {
 
       {!isDeletingThisAccount ? (
         <></>
-        ) : (
+      ) : (
         <>
-        <BasicSection>
-            Mikäli poistat käyttäjätilisi palvelusta, sen kaikki tiedot poistetaan. Vahvistusta kysytään kerran painaessasi "Poista tili". {"\n\n"}
-        </BasicSection>
-        <View style={globalStyles.viewButtons}>
-          <ButtonDelete title="Poista tili" onPress={handleDoubleCheckWhenDeleting} />
-          <ButtonCancel title="Peruuta" onPress={handleDeletingThisAccountCancel} />
-        </View>
+          <BasicSection>
+            Mikäli poistat käyttäjätilisi palvelusta, sen kaikki tiedot
+            poistetaan. Vahvistusta kysytään kerran painaessasi "Poista tili".{" "}
+            {"\n\n"}
+          </BasicSection>
+          <View style={globalStyles.viewButtons}>
+            <ButtonDelete
+              title="Poista tili"
+              onPress={handleDoubleCheckWhenDeleting}
+            />
+            <ButtonCancel
+              title="Peruuta"
+              onPress={handleDeletingThisAccountCancel}
+            />
+          </View>
         </>
-        )}
+      )}
 
       {isConfirmed && (
         <>
-        <BasicSection>
-          Oletko varma? {"\n\n"}
-        </BasicSection>
-        <View style={globalStyles.viewButtons}>
-          <ButtonConfirm title="Vahvista" onPress={handleConfirmWhenDeleting}/>
-          <ButtonCancel title="Peruuta" onPress={handleDeletingThisAccountCancel} />
-        </View>
+          <BasicSection>Oletko varma? {"\n\n"}</BasicSection>
+          <View style={globalStyles.viewButtons}>
+            <ButtonConfirm
+              title="Vahvista"
+              onPress={handleConfirmWhenDeleting}
+            />
+            <ButtonCancel
+              title="Peruuta"
+              onPress={handleDeletingThisAccountCancel}
+            />
+          </View>
         </>
-        )}
+      )}
     </>
   );
 };
 
 export const LogoutFromThisUser = () => {
-  const {authState, setAuthState} = useContext(AuthenticationContext);
-  const {userid, accessToken} = authState;
+  const { authState, setAuthState } = useContext(AuthenticationContext);
+  const { userid, accessToken } = authState;
   const navigation = useNavigation();
-  
+
   const handleLogout = async () => {
     try {
-        clearUserData();
-        setAuthState(null);
-        navigation.navigate('AccountMain');
+      setAuthState(null);
+      await auth.signOut();
+      navigation.navigate("AccountMain");
     } catch (error) {
-        console.error('Virhe uloskirjautumisessa:', error);
-        Alert.alert('Virhe', 'Uloskirjautumisessa tapahtui virhe. Yritä uudelleen.');
+      console.error("Virhe uloskirjautumisessa:", error);
+      Alert.alert(
+        "Virhe",
+        "Uloskirjautumisessa tapahtui virhe. Yritä uudelleen."
+      );
     }
   };
 
   return (
     <>
-      <ButtonNavigate title="Kirjaudu ulos" onPress={handleLogout}/>
+      <ButtonNavigate title="Kirjaudu ulos" onPress={handleLogout} />
     </>
   );
 };
 
 export const MessagingSystem = () => {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
 
   return (
     <>
-      <ButtonNavigate title="Keskustelut" onPress={() => navigation.navigate('MessagesMain')}/>
+      <ButtonNavigate
+        title="Keskustelut"
+        onPress={() => navigation.navigate("MessagesMain")}
+      />
     </>
   );
 };
 
 export const NavigateToThisUsersItems = () => {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
 
   return (
     <>
-      <ButtonNavigate title="Ilmoitukset"  onPress={() => navigation.navigate('MessagesMain')}/>
+      <ButtonNavigate
+        title="Ilmoitukset"
+        onPress={() => navigation.navigate("MessagesMain")}
+      />
     </>
   );
 };
 
 export const NavigateToThisUsersQueue = () => {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
 
   return (
     <>
-      <ButtonNavigate title="Varaukset" onPress={() => navigation.navigate('MessagesMain')}/>
+      <ButtonNavigate
+        title="Varaukset"
+        onPress={() => navigation.navigate("MessagesMain")}
+      />
     </>
   );
 };
 
 export const AccountSystem = () => {
-  const navigation = useNavigation(); 
+  const navigation = useNavigation();
 
   return (
     <>
-      <ButtonNavigate title="Tilin hallinta" onPress={() => navigation.navigate('AccountMaintain')}
+      <ButtonNavigate
+        title="Tilin hallinta"
+        onPress={() => navigation.navigate("AccountMaintain")}
       />
     </>
   );
